1580e45ac3ecf50be0e4afe30a3d71a78ab8baac
Tad Preston <tadpreston@gmail.com>
2012-08-03 10:19:14 -0500

    Add a contact us page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -44,4 +44,18 @@ describe "StaticPages" do
 
   end
 
+  describe "Contact page" do
+
+    it "should have the content 'Contact Us'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', text: 'Contact Us')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/contact'
+      page.should have_selector('title', text: 'Ruby on Rails Tutorial Sample App | Contact Us')
+    end
+
+  end
+
 end
