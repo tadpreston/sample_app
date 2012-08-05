b85da687bcc425a1286fbb761a75b340083c5efa
Tad Preston <tadpreston@gmail.com>
2012-08-04 23:33:49 -0500

    Add additional test for the before_save filter and refactor it

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
 
-  before_save { |user| user.email = email.downcase }
+  before_save { self.email.downcase! }
 
   validates :name, presence: true, length: { maximum: 50 }
   ValidEmailRegex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
