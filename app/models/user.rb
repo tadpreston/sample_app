ba6f3d7071ed8f854421d3dc0d967e16fa750dcc
Tad Preston <tadpreston@gmail.com>
2012-08-04 22:17:42 -0500

    Add validations to the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,8 @@
 
 class User < ActiveRecord::Base
   attr_accessible :email, :name
+
+  validates :name, presence: true, length: { maximum: 50 }
+  ValidEmailRegex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: ValidEmailRegex }, uniqueness: { case_sensitive: false }
 end
